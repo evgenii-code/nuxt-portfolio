4adebb2f3d7b95f19bcc20ec67f956d6e1fc0afc
evgenii-code <osipenkov.evgenii@gmail.com>
2020-06-15 20:27:55 +0300

    vue showdown

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,12 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    {
+      src: '~plugins/vue-showdown.js',
+      // ssr:false
+    },
+  ],
   /*
    ** Nuxt.js dev-modules
    */
